8bc764de8e898f3758d7457054f7575f2838389b
Riz Michael Sim <rizsim214@gmail.com>
2022-06-09 21:23:28 +0800

    Working CRUD - Added SHOW UPDATE DELETE methods

--- src/main/java/com/scheduler/sbclinic/rest/AccountsController.java
+++ src/main/java/com/scheduler/sbclinic/rest/AccountsController.java
@@ -9,6 +9,7 @@ import com.scheduler.sbclinic.models.Account;
 
 public class AccountsController implements ModelDriven<Object> {
     // private static final long serialVersionUID = 1L; 
+    private String id;
     private Object model;
     private AccountsRepository accountRepository = new AccountsRepository();
     private Account account = new Account();
@@ -17,17 +18,43 @@ public class AccountsController implements ModelDriven<Object> {
         map = accountRepository.findAllAccounts();
     }
 
-    public HttpHeaders index() {
+    public HttpHeaders index() throws Exception{
         model = map;
-        return new DefaultHttpHeaders("index").disableCaching();
+        return new DefaultHttpHeaders("index");
     }    
     
-    public HttpHeaders create() {
-        accountRepository.save(account);
+    public HttpHeaders create() throws Exception{
+        accountRepository.saveAccount(account);
         return new DefaultHttpHeaders("create");
+    }
+
+    //GET accounts/id/
+    public HttpHeaders show() throws Exception{
+        model = accountRepository.findAccountById(getId());
+        return new DefaultHttpHeaders("show");
+    }
+
+    public HttpHeaders update() throws Exception{
+        account = map.get(getId());
+        System.out.println(account.toString());
+        accountRepository.updateAccount(getId(), account);
+        return new DefaultHttpHeaders("update").disableCaching();
+    }
+
+    public HttpHeaders destroy() throws Exception{
+        accountRepository.deleteAccount(getId());
+        return new DefaultHttpHeaders("destroy");
     }
     @Override
 	public Object getModel() {
 		return (model != null ? model : account);
+	}
+
+    public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		model = accountRepository.findAccountById(id);
+		this.id = id;
 	}
 }
--- src/main/java/com/scheduler/sbclinic/rest/AccountsRepository.java
+++ src/main/java/com/scheduler/sbclinic/rest/AccountsRepository.java
@@ -18,6 +18,10 @@ public class AccountsRepository {
     
     public AccountsRepository(){}
 
+    public Account findAccountById(String id) {
+        return map.get(id);
+    }
+
     public Map<String, Account> findAllAccounts(){
         Connection conn = null;
         PreparedStatement prep = null;
@@ -53,7 +57,7 @@ public class AccountsRepository {
             return map;
     }
     
-    public boolean save(Account account) {
+    public boolean saveAccount(Account account) {
         Connection conn = null;
         Statement prep = null;
         
@@ -66,7 +70,59 @@ public class AccountsRepository {
                 prep = conn.createStatement();
                 String sql = "INSERT INTO users (email_add, pass_word, first_name, last_name, contact_info, user_role, user_status) VALUES('"+account.getEmail()+"','"+account.getUserPassword()+"','"+account.getFirstName()+"', '"+account.getLastName()+"', '"+account.getContactInfo()+"', '"+account.getUserRole()+"','"+account.getUserStatus()+"')";
                 prep.executeUpdate(sql);
-               
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }finally{
+            if(prep != null) try { prep.close();} catch (SQLException ignore) {}
+            if(conn != null) try { conn.close();} catch (SQLException ignore) {}
+        }
+    }
+
+    public boolean updateAccount(String id, Account obj) {
+        Connection conn = null;
+        Statement prep = null;
+        
+        try {
+            String URL = "jdbc:mysql://localhost:3306/sbclinicdb?useTimezone=true&serverTimezone=UTC";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, "root", "password123");
+
+            if(conn != null){
+                prep = conn.createStatement();
+                String sql = "UPDATE users SET email_add = '"+obj.getEmail()+"', pass_word = '"+obj.getUserPassword()+"', first_name = '"+obj.getFirstName()+"', last_name = '"+obj.getLastName()+"', contact_info = '"+obj.getContactInfo()+"', user_role = '"+obj.getUserRole()+"', user_status = '"+obj.getUserStatus()+"' WHERE userID = '"+ id +"' ";
+                prep.executeUpdate(sql);
+
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }finally{
+            if(prep != null) try { prep.close();} catch (SQLException ignore) {}
+            if(conn != null) try { conn.close();} catch (SQLException ignore) {}
+        }
+    }
+    
+    public boolean deleteAccount(String id) {
+        Connection conn = null;
+        Statement prep = null;
+        
+        try {
+            String URL = "jdbc:mysql://localhost:3306/sbclinicdb?useTimezone=true&serverTimezone=UTC";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, "root", "password123");
+
+            if(conn != null){
+                prep = conn.createStatement();
+                String sql = "DELETE FROM users WHERE userID = '"+ id +"'";
+                prep.executeUpdate(sql);
                 return true;
             }else{
                 return false;
